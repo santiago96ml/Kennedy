2d81d518345f5124bc9209fe8b21cc488b409907
santiago96ml <mercadolunasantiago944@gmail.com>
2026-01-26 18:28:40 -0300

    Actualización del proyecto

--- Kennedy/src/lib/supabaseClient.ts
+++ Kennedy/src/lib/supabaseClient.ts
@@ -6,6 +6,7 @@ const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYm
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
+    flowType: 'pkce',
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: true
